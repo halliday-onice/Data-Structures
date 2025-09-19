253a720d96c382382d7fa9d93b590d8545e9d119
halliday-onice <jonatashalliday1206@gmail.com>
2025-09-18 21:25:25 -0300

    updating the repository

--- Binary search tree/recursive_bst.py
+++ Binary search tree/recursive_bst.py
@@ -57,8 +57,42 @@ class BinarySearchTree:
         if self.root == None:
             self.root = Node(value)
         self.__r_insert(self.root, value)
+    
 
-my_tree = BinarySearchTree()
+    
+    #a recursive method for finding a minimum value in a binary search tree
+    def min_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+
+
+    def __delete_node(self, current_node, value):
+        if current_node == None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else: #case where the child we are removing has child node on each side
+            #look for the right subtree of the node that is in current_node
+            if current_node.left == None and current_node.right == None: #it is a leaf node
+                return None
+            elif current_node.left == None: #do not have a leaf on the left, but it has one on the right
+                current_node = current_node.right
+            elif current_node.right == None:#has one node on the right but not on the left
+                current_node = current_node.left
+            else: # or has one node on the left and one on the right
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min 
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+        return current_node
+    
+    def delete_node(self, value):
+        self.root = self.__delete_node(self.root, value)
+
+
+# my_tree = BinarySearchTree()
 # my_tree.insert(47)
 # my_tree.insert(21)
 # my_tree.insert(76)
@@ -74,10 +108,54 @@ my_tree = BinarySearchTree()
 # print(my_tree.r_contains(17))
 
 #recursive insert
+# my_tree.r_insert(2)
+# my_tree.r_insert(1)
+# my_tree.r_insert(3)
+
+# print('Root', my_tree.root.value)
+# print('Root-> Left', my_tree.root.left.value)
+# print('Root-> Right', my_tree.root.right.value)
+
+my_tree = BinarySearchTree()
 my_tree.r_insert(2)
 my_tree.r_insert(1)
 my_tree.r_insert(3)
 
-print('Root', my_tree.root.value)
-print('Root-> Left', my_tree.root.left.value)
-print('Root-> Right', my_tree.root.right.value)
+"""
+       2
+      / \
+     1   3
+"""
+
+print("root:", my_tree.root.value)
+print("root.left =", my_tree.root.left.value)
+print("root.right =", my_tree.root.right.value)
+
+
+my_tree.delete_node(2)
+
+"""
+       3
+      / \
+     1   None
+"""
+
+
+print("\nroot:", my_tree.root.value)
+print("root.left =", my_tree.root.left.value)
+print("root.right =", my_tree.root.right)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+	root: 2
+	root.left = 1
+	root.right = 3
+
+	root: 3
+	root.left = 1
+	root.right = None
+
+"""
---
+++ MergeSort/merge_two_sorted_list.py
@@ -0,0 +1,84 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+
+
+            
+            self.tail = new_node
+
+
+        self.length += 1
+
+    # WRITE MERGE METHOD HERE #
+    #                         #
+    #                         #
+    #                         #
+    #                         #
+    ###########################
+    def merge_LL(self, other_list):
+        dummy = Node(0)
+        current = dummy #keep track where we are in the new list, 
+        
+        while self.head is not None and other_list.head is not None:
+            if(self.head < other_list.head):
+                current.next = self.head
+                self.head = self.head.next
+            else:
+                current.next = other_list.head
+                self.other_head = self.head.next
+                    
+            current   = current.next
+            # 
+
+l1 = LinkedList(1)
+l1.append(3)
+l1.append(5)
+l1.append(7)
+
+
+l2 = LinkedList(2)
+l2.append(4)
+l2.append(6)
+l2.append(8)
+
+l1.merge_LL(l2)
+
+l1.print_list()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    1 
+    2 
+    3 
+    4 
+    5 
+    6 
+    7
+    8
+
+"""
